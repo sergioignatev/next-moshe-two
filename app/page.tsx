37413fba6cc29412970a78ec89c09791350fa139
SergioIgnatev <ignatevsergey17@gmail.com>
2023-11-06 21:05:29 +0300

    h

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>THIS IS THE TEST</h1>
+      <h1 className="bg-[red]">THIS IS THE TEST </h1>
     </main>
   )
 }
